a2889d4edd370a5edf6069ac0165e58e24084d63
Jasleen <jasleensekhon867@gmail.com>
2025-08-15 21:06:48 -0700

    method to calculate price

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ class Smoothie {
         this.extras = extras;     
         this.price = this.calculatePrice(); 
     }
+// Method: Calculate Price
+ 
+    calculatePrice() {
+        let basePrice = 0;
+
+        // Determine base price according to size
+        if (this.size === "small") basePrice = 3;
+        if (this.size === "medium") basePrice = 5;
+        if (this.size === "large") basePrice = 7;
+        
+        // Add $1 for each extra ingredient selected
+        basePrice += this.extras.length * 1;
+
+        return basePrice; // Return the total price
+    }
